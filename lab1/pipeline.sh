e3329e02f9b100d49fb449808564b69038bd4c90
Dk-A-r <114184125+Dk-A-r@users.noreply.github.com>
2023-05-11 19:36:45 +0300

    Update pipeline.sh

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+TRIALNAME0="data_creation.py"
+TRIALNAME1="data_preprocessing.py"
+TRIALNAME2="model_preparation.py"
+TRIALNAME3="model_testing.py"
+
+
+chmod a+rwx $TRIALNAME0
+chmod a+rwx $TRIALNAME1
+chmod a+rwx $TRIALNAME2
+chmod a+rwx $TRIALNAME3
+chmod a+rwx $TRIALNAME4
+
+python data_creation.py
+python data_preprocessing.py
+python model_preparation.py
+python model_testing.py
