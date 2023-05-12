8f6caa0409c264483c8265f47cdbf4d7af1dbfff
Dk-A-r <114184125+Dk-A-r@users.noreply.github.com>
2023-05-12 10:44:59 +0300

    Update data_creation.py

--- lab1/data_creation.py
+++ lab1/data_creation.py
@@ -1,139 +1,68 @@
 import pandas as pd
 import numpy as np
+import os
+from sklearn.model_selection import train_test_split
 
-def true_fun(x, a=np.pi, b = 0, f=np.sin): 
-    """Генерация произвольной зависимости   
-    Входные переменные:
-    ===========
-    x: массив данных из которых будет генерироваться зависимость
-    a: коэффициент на который входные данные будут умножаться
-    если а - список, то это коэффициенты в полиномиальной зависимости
-    так, а = [1,2,3] позволит сгенерировать зависимость вида 1*x+ 2*x^2 +3*x^3 
-    b: коэффициент который будет добавлен к данным (постоянная прибавочка)
-    f: функция которая будет применена к зависимости. Можно задать списком. Тогда это будут разные колонки
-    """
-    x = np.atleast_1d(x)[:] # убеждаемся что данные - одномерный массив
-    a = np.atleast_1d(a)
-    
-    if f is None: f = lambda x:x # если функция не задана (None) то ничего не происходит
-    x = np.sum([ai*np.power(x, i+1) for i,ai in enumerate(a)],axis=0) # домнажаем входные данные на коэффициенты (и если надо возводим в степень)
+x_lin = np.random.randn(250)*10
+x_pol = np.random.randn(250)
+x_sin = np.random.randn(1000)
 
-    return f(x+ b)
-  
-  
-def noises(shape , noise_power):
-    """Генерация случайного шума   
-    Входные переменные:
-    ===========
-    shape: размерность массива данных
-    noise_power: коэффициент ~ сила шума
-    """
-    return np.random.randn(*shape) *noise_power # библиотека numpy может генерировать случайные числа. 
-                                                # в данном случае - нормальное распределение (среднее = 0, стандартное отклонение = 1 )
-    
-   
-def dataset(a, b, f = None,  N = 250, x_max =1, noise_power = 0, random_x = True,  seed = 42):
-    """Генерация набора данных   
-    Входные переменные:
-    ===========
-    a: коэффициент на который входные данные будут умножаться
-    если а - список, то это коэффициенты в полиномиальной зависимости
-    так, а = [1,2,3] позволит сгенерировать зависимость вида 1*x+ 2*x^2 +3*x^3 
-    b: коэффициент который будет добавлен к данным (постоянная прибавочка)
-    f: функция которая будет применена к зависимости. Можно задать списком. Тогда это будут разные колонки
-    N: количество точек данных
-    x_max: максимальное значение данных
-    noise_power: коэффициент ~ сила шума
-    random_x: как будут распределены данные (линейно, или случайно)
-    seed: фиксированный сид случайных чисел (для повторяемости)
-    """
+y_lin = x_lin*8 + 7 #значение пути при постоянной скорости x_lin
+                    #за время 8 при старте в точке 7
 
-    np.random.seed(seed) # фиксируем случайный seed
-    
-    if random_x:# если мы хотим случайно распределить данные
-        x = np.sort(np.random.rand(N))*x_max # то x будет N случайных числе из диапазона от 0 до x_max
-    else: # иначе
-        x = np.linspace(0,x_max,N) # х это равномерно распределенные N чисел из диапазона от 0 до x_max
-    
-    y_true = np.array([]) # создаем пустой массив который будет "наполнять" зависимостями
-    
-    for f_ in np.append([], f): # если f - задана списком, то мы учтем все варианты
-        y_true=np.append(y_true, true_fun(x, a, b, f_)) # применяем описанную выше функцию true_fun
-    
-    y_true = y_true.reshape(-1,N).T
-    y = y_true + noises(y_true.shape , noise_power) # добавляем шум
+y_pol = 2000 - x_pol**2*10/2  #приближенная высота, на которой находится тело
+                              #при свободном падении с высоты 2000 м через
+                              #x_pol секунд
 
-    return y, y_true, np.atleast_2d(x).T # возвращаем зашумленные значения зависимостей, зависимости без шума, и массив входных данных
-  
-#Генерация линейной - зависимости 
-lin_y, lin_y_true, x = dataset(a=3,
-                    b=7,
-                    f=[lambda x: x, lambda k: k**2],
-                    N=250,
-                    x_max=10,
-                    noise_power=3,
-                   
-                    random_x=True,
-                    seed=42
-                    )
-lin_indexes = list(range(250))
-lin_col_names = ['x', 'x**2']
-df_lin = pd.DataFrame(data = lin_y, 
-              index = lin_indexes, 
-              columns = lin_col_names)
-df_lin_true = pd.DataFrame(data = lin_y, 
-              index = lin_indexes, 
-              columns = lin_col_names)
-
-#Генерация гармонической зависимости
-
-sin_y, sin_y_true, x = dataset(a=3,
-                b=0.333,
-                f=[np.sin, np.cos],
-                N=1000,
-                x_max=10,
-                noise_power=0.1,
-                random_x=True,
-                seed=42
-                )
-sin_indexes = list(range(1000))
-sin_col_names = ['sin', 'cos']
-df_sin = pd.DataFrame(data = sin_y, 
-              index = sin_indexes, 
-              columns = sin_col_names)
-df_sin_true = pd.DataFrame(data = sin_y_true, 
-              index = sin_indexes, 
-              columns = sin_col_names)
+y_sin =  ((10*np.cos(x_sin)*10)**2 \
+         + (10*np.sin(x_sin) - 10*10)**2))**0.5 #примерная скорость полета при параболическом движении
+                                                #под углом x_sin радиан с начальной скоростью 10 после 10 секунд
 
 
-#Генерация полинома второй степени
+X_lin_train, X_lin_test, y_lin_train, y_lin_test = train_test_split(
+              x_lin, y_lin, test_size=0.33, random_state=42)
 
-pol_y, pol_y_true, x = dataset(a=[0.32, 0.75],
-                b=7,
-                f=[lambda x: x, lambda k: k**2],
-                N=250,
-                x_max=10,
-                noise_power=3,
-                random_x=True,
-                seed=42
-                )
-pol_indexes = list(range(250))
-pol_col_names = ['x', 'x**2']
-df_pol = pd.DataFrame(data = pol_y, 
-              index = pol_indexes, 
-              columns = pol_col_names)
-df_pol_true = pd.DataFrame(data = pol_y_true, 
-              index = pol_indexes, 
-              columns = pol_col_names)
+X_pol_train, X_pol_test, y_pol_train, y_pol_test = train_test_split(
+              x_pol, y_pol, test_size=0.33, random_state=42)
+
+X_sin_train, X_sin_test, y_sin_train, y_sin_test = train_test_split(
+              x_sin, y_sin, test_size=0.33, random_state=42)
+
+#Тренировочные данные
+df_tr_lin = pd.DataFrame('Скорость': X_lin_train,
+                        'Путь': y_lin_train)
+df_tr_pol = pd.DataFrame('Время': X_pol_train,
+                        'Высота': y_pol_train)
+df_tr_sin = pd.DataFrame('Угол': X_sin_train,
+                        'Скорость': y_sin_train)
+
+#Тестовые данные
+df_ts_lin = pd.DataFrame('Скорость': X_lin_test,
+                        'Путь': y_lin_test)
+df_ts_pol = pd.DataFrame('Время': X_pol_test,
+                        'Высота': y_pol_test)
+df_ts_sin = pd.DataFrame('Угол': X_sin_test,
+                        'Скорость': y_sin_test)
+
+#Проверить, есть ли директория для тестовых и тренировочных, если нет, то создать
+
+def creatdir(*paths):
+    for i in paths:
+        isExists = os.path.exists(i)
+        if not is Exists:
+            os.makedirs(i)
+            
+createdir('train', 'test')
+
 
 #Сохраняем в файлы
 
 #Сначала тренировочные
-df_lin.to_csv('train/lin.csv', index=False)
-df_sin.to_csv('train/sin.csv', index=False)
-df_pol.to_csv('train/pol.csv', index=False)
+df_tr_lin.to_csv('train/lin.csv', index=False)
+df_tr_sin.to_csv('train/sin.csv', index=False)
+df_tr_pol.to_csv('train/pol.csv', index=False)
 
 #Потом тестовые
-df_lin_true.to_csv('test/lin_true.csv', index=False)
-df_sin_true.to_csv('test/sin_true.csv', index=False)
-df_pol_true.to_csv('test/pol_true.csv', index=False)
+df_ts_lin_true.to_csv('test/lin_true.csv', index=False)
+df_ts_sin_true.to_csv('test/sin_true.csv', index=False)
+df_ts_pol_true.to_csv('test/pol_true.csv', index=False)
