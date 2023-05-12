88441c44ef4b5af4c7338fa18609b741e7aa7eb0
Dk-A-r <114184125+Dk-A-r@users.noreply.github.com>
2023-05-12 17:15:01 +0300

    Update data_preprocessing.py

--- lab1/data_preprocessing.py
+++ lab1/data_preprocessing.py
@@ -4,32 +4,22 @@ import numpy as np
 
 scaler = StandardScaler()
 
-#Линейная
-df_ts_lin = pd.read_csv('test/lin_true.csv')#, header=None)
-df_tr_lin = pd.read_csv('train/lin.csv')#, header=None)
+#1
+df_1_ts = pd.read_csv('test/1.csv')#, header=None)
+df_1_tr = pd.read_csv('train/1.csv')#, header=None)
 
-#Полиномиальная
-df_ts_pol = pd.read_csv('test/pol_true.csv')#, header=None)
-df_tr_pol = pd.read_csv('train/pol.csv')#, header=None)
+#2
+df_2_ts = pd.read_csv('test/2.csv')#, header=None)
+df_2_tr = pd.read_csv('train/2.csv')#, header=None)
 
-#Гармоническая
-df_ts_sin=pd.read_csv('test/sin_true.csv')#, header=None)
-df_tr_sin = pd.read_csv('train/sin.csv')#, header=None)
 
 #Стандартизируем и выводим в csv
-lin_tr_std = scaler.fit_transform(df_tr_lin[['Скорость']])
-lin_ts_std = scaler.fit_transform(df_ts_lin[['Скорость']])
-np.savetxt('train/lin_std.csv', lin_tr_std, delimiter=',')
-np.savetxt('test/lin_std.csv', lin_ts_std, delimiter=',')
+std_tr_1 = scaler.fit_transform(df_1_tr[['Время']])
+std_ts_1 = scaler.fit_transform(df_1_ts[['Время']])
+np.savetxt('train/1_std.csv', lin_tr_std, delimiter=',')
+np.savetxt('test/1_std.csv', lin_ts_std, delimiter=',')
 
-pol_tr_std = scaler.fit_transform(df_tr_pol[['Время']])
-pol_ts_std = scaler.fit_transform(df_ts_pol[['Время']])
-np.savetxt('train/pol_std.csv', pol_tr_std, delimiter=',')
-np.savetxt('test/pol_std.csv', pol_ts_std, delimiter=',')
-
-sin_tr_std = scaler.fit_transform(df_tr_sin[['Угол']])
-sin_ts_std = scaler.fit_transform(df_ts_sin[['Угол']])
-np.savetxt('train/sin_std.csv', sin_tr_std, delimiter=',')
-np.savetxt('test/sin_std.csv', sin_ts_std, delimiter=',')
-
-
+std_tr_2 = scaler.fit_transform(df_2_tr[['Время']])
+std_ts_2 = scaler.fit_transform(df_2_ts[['Время']])
+np.savetxt('train/2_std.csv', lin_tr_std, delimiter=',')
+np.savetxt('test/2_std.csv', lin_ts_std, delimiter=',')
