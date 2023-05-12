71845927f850a9987db2c35f3e9b18f31f1227c7
Dk-A-r <114184125+Dk-A-r@users.noreply.github.com>
2023-05-12 20:30:31 +0300

    Update model_testing.py

--- lab1/model_testing.py
+++ lab1/model_testing.py
@@ -1,1 +1,21 @@
+import pickle
+import sklearn
 
+
+pkl_filename = "pickle_model.pkl"
+with open(pkl_filename, 'rb') as file: 
+    pickle_model = pickle.load(file)
+
+df_ts_1 = pd.read_csv('test/1.csv')
+df_ts_2 = pd.read_csv('test/2.csv')
+df_ts = pd.concat([df_ts_1, df_ts_2])
+
+Y_test = df_ts['Высота'].to_numpy().reshape(-1, 1)
+X_test = np.concatenate(
+    (np.genfromtxt('test/1_std.csv', delimiter=','),
+     np.genfromtxt('test/2_std.csv', delimiter=','))
+).reshape(-1, 1)
+
+score = pickle_model.score(X_test, Y_test) 
+print("Test score: {0:.2f} %".format(100 * score)) 
+Ypredict = pickle_model.predict(X_test)
