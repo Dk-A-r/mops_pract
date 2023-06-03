56bc0f9287f785401de4b6d44b8f533dc4dbb2cc
Dk-A-r <114184125+Dk-A-r@users.noreply.github.com>
2023-06-03 17:34:15 +0300

    Update pipeline.sh

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-TRIALNAME0="./data_creation.py"
-TRIALNAME1="./data_preprocessing.py"
-TRIALNAME2="./model_preparation.py"
-TRIALNAME3="./model_testing.py"
+TRIALNAME0="data_creation.py"
+TRIALNAME1="data_preprocessing.py"
+TRIALNAME2="model_preparation.py"
+TRIALNAME3="model_testing.py"
 
 
 sudo chmod +x $TRIALNAME0
@@ -12,7 +12,7 @@ sudo chmod +x $TRIALNAME2
 sudo chmod +x $TRIALNAME3
 sudo chmod +x $TRIALNAME4
 
-python ./data_creation.py
-python ./data_preprocessing.py
-python ./model_preparation.py
-python ./model_testing.py
+python data_creation.py
+python data_preprocessing.py
+python model_preparation.py
+python model_testing.py
