de6f5a1910e065000d0ea519613f2fe656a6d38c
Dk-A-r <114184125+Dk-A-r@users.noreply.github.com>
2023-06-06 20:34:25 +0300

    Update pipeline.sh

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo pip install pandas
+sudo pip install sklearn
+sudo pip install numpy
+
 TRIALNAME0=`sudo find / -path \*mops_pract-main/lab1/data_creation.py`
 TRIALNAME1=`sudo find / -path \*mops_pract-main/lab1/data_preprocessing.py`
 TRIALNAME2=`sudo find / -path \*mops_pract-main/lab1/model_preparation.py`
