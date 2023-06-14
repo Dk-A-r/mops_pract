44fcdcb67b8e2afb953e599971a3ede2647b9e51
Dk-A-r <114184125+Dk-A-r@users.noreply.github.com>
2023-06-14 22:09:07 +0300

    Update train_model.py

--- lab2/train_model.py
+++ lab2/train_model.py
@@ -1,14 +1,15 @@
-import pandas as pd
-
-#read .csv into DF
-data_train = pd.read_csv('data_train.csv')
-X_train = data_train[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']].values
-y_train = data_train['Survived'].values
-
-#load ML model
+from numpy import genfromtxt
 from sklearn.linear_model import LogisticRegression
-model = LogisticRegression(max_iter=100_000).fit(X_train, y_train)
 
-#save and dump fitted model
-import pickle
-pickle.dump(model, open('model.pkl', 'wb'))
+X_train = genfromtxt('x_train.csv', delimiter=',')
+y_train = genfromtxt('y_t.csv', delimiter=',')
+
+model = LogisticRegression()
+
+model.fit(x_train, y_train)
+
+#Saving
+pkl_filename = "pickle_model.pkl" 
+with open(pkl_filename, 'wb') as file: 
+    pickle.dump(model, file)
+
